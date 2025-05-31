4d5ba208eddd2a0c43acde8838bf59ed800dbc32
nikhil <imnikhilchouksey@gmail.com>
2025-05-31 18:34:34 +0530

    Homepage completed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -50,7 +50,7 @@ const HomePage = () => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, type: 'spring' }}
           viewport={{ once: true }}
-          whileHover={{ scale: 1.1}}
+          whileHover={{ scale: 1.1 }}
           className="w-32 h-32 md:h-64 md:w-64 rounded-full p-1 bg-gradient-to-tr from-blue-500 to-purple-600 shadow-lg"
         >
           <img
@@ -63,7 +63,7 @@ const HomePage = () => {
           Hello I am
           <span className='text-4xl text-blue-500'>
             <Typewriter
-              words={[' Nikhil!', ' a web developer', ' problem solver']}
+              words={[' Nikhil!', ' a web developer', ' a problem solver']}
               loop={true}
               cursor
               cursorStyle="_"
@@ -75,23 +75,26 @@ const HomePage = () => {
         </h1>
         <button className='bg-white rounded-full px-4 py-2 text-black font-bold border-4 transition shadow transform duration-300 hover:scale-110 hover:shadow-blue-500'>Download Resume</button>
       </div>
-      <div className="w-[44vh] md:hidden flex justify-center items-center text-center px- py-2 fixed bottom-0 bg-gray-900 rounded-full mb-10  gap-1">
+      <div className="w-auto h-[8dvh] md:hidden flex justify-center items-center text-center px-2 py-2 fixed bottom-0 bg-gray-900 rounded-full mb-10  gap-1">
         {tabs.map((tab) => (
           <button
             key={tab.id}
             onClick={() => handleTabClick(tab.id)}
-            className={`${activeTab === tab.id ? "text-blue-500" : "text-white/70"
-              } relative px-1 text-sm font-medium transition hover:text-white`}
+            className={`
+    relative px-1 py-1 text-sm font-medium transition-all duration-300
+    ${activeTab === tab.id ? "text-black" : "text-white opacity-40 hover:opacity-100"}
+  `}
           >
             {activeTab === tab.id && (
               <motion.span
                 layoutId="bubble"
-                className="absolute inset-0 z-10 bg-white mix-blend-difference rounded-full"
+                className="absolute inset-0 z-0 bg-white rounded-full"
                 transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
               />
             )}
-            {tab.label}
+            <span className="relative z-10">{tab.label}</span>
           </button>
+
         ))}
       </div>
     </div>
