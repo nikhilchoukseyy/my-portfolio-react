637d9784d11a728aded86294b9301d0c00392960
nikhil <imnikhilchouksey@gmail.com>
2025-06-01 15:53:43 +0530

    some changes in homepage done

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import { motion } from "framer-motion";
 import { Typewriter } from 'react-simple-typewriter';
 import avatarPhoto from '../assets/github-profile-photo.jpeg'
+import { Download } from 'lucide-react';
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
 let tabs = [
   { id: 'home', label: 'Home' },
@@ -21,9 +25,9 @@ const HomePage = () => {
   }
 
   return (
-    <div className='bg-gray-950 h-[100vh] flex flex-col items-center text-white'>
-      <nav className='h-[10vh] w-[100%] justify-center px-8 py-8 mb-12 flex flex-row items-center text-center md:justify-between'>
-        <span className='text-2xl'>nikhil<span className='text-blue-500'>chouksey</span></span>
+    <div className='bg-gray-950 h-[100vh] flex flex-col items-center text-white font-mono'>
+      <nav className='h-[10dvh] w-[100%] justify-center px-8 py-8 mb-8 flex flex-row items-center text-center md:justify-between'>
+        <span className='text-2xl '>nikhil<span className='text-blue-500'>chouksey</span></span>
         <div className="hidden sm:block flex justify-center items-center p-4 space-x-3">
           {tabs.map((tab) => (
             <button
@@ -44,7 +48,7 @@ const HomePage = () => {
           ))}
         </div>
       </nav>
-      <div className='flex flex-col items-center justify-center text-center h-auto gap-12'>
+      <div className='flex flex-col items-center justify-center text-center h-auto gap-12 md:gap-8'>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -59,11 +63,15 @@ const HomePage = () => {
             className="w-full h-full object-cover rounded-full border-4 border-white"
           />
         </motion.div>
-        <h1 className='gap-2 text-4xl flex flex-col text-center items-center justify-center md:flex-row'>
-          Hello I am
-          <span className='text-4xl text-blue-500'>
+        <div className='h-auto w-auto flex flex-col md:flex-row md:items-baseline items-center justify-center gap-1 md:gap-2'>
+          <h1 className='text-3xl md:text-4xl font-bold'>Hello I am</h1>
+
+          <div
+            className='h-[40px] md:h-[48px] text-center md:text-left text-2xl font-thin md:text-4xl text-blue-500 overflow-hidden'
+            style={{ minWidth: '360px' }} 
+          >
             <Typewriter
-              words={[' Nikhil!', ' a web developer', ' a problem solver']}
+              words={[' Nikhil chouksey', ' a web developer', ' a problem solver']}
               loop={true}
               cursor
               cursorStyle="_"
@@ -71,17 +79,31 @@ const HomePage = () => {
               deleteSpeed={100}
               delaySpeed={3000}
             />
-          </span>
-        </h1>
-        <button className='bg-white rounded-full px-4 py-2 text-black font-bold border-4 transition shadow transform duration-300 hover:scale-110 hover:shadow-blue-500'>Download Resume</button>
+          </div>
+        </div>
+
+
+        <button className="flex flex-row bg-white rounded-full px-4 py-2 text-black font-extrabold hover:border-2 transition transform duration-300 hover:scale-110 hover:shadow-blue-500 cursor-pointer hover:border-blue-500 active:shadow-blue-100 active:scale-110 md:mt-4 md:mb-4 ">
+          <a className="flex flex-row gap-2 "
+            href=""
+            download="Nikhil_Chouksey_Resume">
+            <span>Download Resume</span>
+            <Download />
+          </a>
+        </button>
+        <div className=' flex flex-row gap-4  '>
+          <button className='text-2xl'><FaGithub /></button>
+          <button className='text-2xl'><CiLinkedin /></button>
+          <button className='text-2xl'><MdOutlineEmail /></button>
+        </div>
       </div>
-      <div className="w-auto h-[8dvh] md:hidden flex justify-center items-center text-center px-2 py-2 fixed bottom-0 bg-gray-900 rounded-full mb-10  gap-1">
+      <div className="w-max-auto h-[8dvh] md:hidden flex justify-center items-center text-center px-2 py-2 fixed bottom-0 bg-gray-900 rounded-full mb-12 ">
         {tabs.map((tab) => (
           <button
             key={tab.id}
             onClick={() => handleTabClick(tab.id)}
             className={`
-    relative px-1 py-1 text-sm font-medium transition-all duration-300
+    relative px-1 py-1 text-xs font-medium transition-all duration-300
     ${activeTab === tab.id ? "text-black" : "text-white opacity-40 hover:opacity-100"}
   `}
           >
--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProjectPage = () => {
   return (
-    <div className='h-[100vh] bg-gray-950 text-center flex items-center justify-center text-white'>
+    <div className='h-[100vh] bg-gray-900 text-center flex items-center justify-center text-white'>
       ProjectPage
     </div>
   )
